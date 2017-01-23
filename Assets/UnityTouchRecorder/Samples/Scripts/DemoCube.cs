e9600da72a36dcc7741d58ee01aacc7a8852e645
thedoritos <thedoritos@gmail.com>
2017-01-24 01:28:00 +0900

    Add demo to get Input.touches

---
+++ Assets/UnityTouchRecorder/Samples/Scripts/DemoCube.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnityTouchRecorder.Sample
+{
+    public class DemoCube : MonoBehaviour
+    {
+        [SerializeField]
+        private int fingerId;
+
+        private Vector3 rotationVector;
+
+        void Update()
+        {
+            rotationVector = Vector3.Lerp(this.rotationVector, Vector3.zero, 0.3f);
+            transform.Rotate(rotationVector);
+
+            var touches = Input.touches.Where(t => t.fingerId == fingerId);
+            if (touches.Count() == 0)
+            {
+                return;
+            }
+
+            var touch = touches.First();
+            rotationVector = touch.deltaPosition * touch.deltaTime * 10;
+        }
+    }
+}
---
+++ Assets/UnityTouchRecorder/Samples/Scripts/DemoSceneController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace UnityTouchRecorder.Sample
+{
+    public class DemoSceneController : MonoBehaviour
+    {
+        public void OnButtonPressed()
+        {
+            Debug.Log("DemoSceneController#OnButtonPressed");
+        }
+    }
+}
